21b131ddd0d1d24ba83678351648e455f14b44ce
danuseptiadi <87447976+danuseptiadi@users.noreply.github.com>
2022-01-13 21:57:02 +0700

    Delete TugasAsistensi directory

--- TugasAsistensi/src/Controller/AdminController.java
+++
@@ -1,9 +0,0 @@
-package Controller;
-
-import AllObject.AllObjectModel;
-
-public class AdminController {
-    public int checkAdmin(String nama, String password){
-        return AllObjectModel.adminModel.checkAdmin(nama, password);
-    }
-}
--- TugasAsistensi/src/Model/ModelAbstract.java
+++
@@ -1,7 +0,0 @@
-package Model;
-
-public abstract class ModelAbstract {
-    
-    public abstract <Any> Any getData();
-    
-}
